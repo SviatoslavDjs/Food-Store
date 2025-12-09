880f3e3ee55ec6575f934f9a42fd17874b77e7ec
Sviatoslav <svyatoslavdjs@gmail.com>
2025-12-09 14:49:56 +0200

    Add Readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Food-Store
+This project demonstrates a web interface with interactive modal windows and tab navigation, calculating and adaptation. Built with HTML, SCSS, and JavaScript.
---
+++ dist/js/script.js
@@ -0,0 +1,119 @@
+/******/ (() => { // webpackBootstrap
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.style.display = "none";
+      item.classList.remove("fade");
+    });
+    tabs.forEach(tab => {
+      tab.classList.remove("tabheader__item_active");
+    });
+  }
+  function showTabContent(i = 0) {
+    tabsContent[i].style.display = "block";
+    tabsContent[i].classList.add("fade");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+  hideTabContent();
+  showTabContent();
+  tabsParent.addEventListener("click", event => {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+  // timer
+  const deadLine = "2025-12-20";
+  function getTameRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor(t / (1000 * 60 * 60) % 24);
+      minutes = Math.floor(t / 1000 / 60 % 60);
+      seconds = Math.floor(t / 1000 % 60);
+    }
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      second: seconds
+    };
+  }
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+    function updateClock() {
+      const t = getTameRemaining(endtime);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.second);
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock(".timer", deadLine);
+
+  //modal
+  const modal = document.querySelector(".modal");
+  const btn = document.querySelectorAll("[data-modal]");
+  const close = document.querySelector("[data-close]");
+  btn.forEach(button => {
+    button.addEventListener("click", () => {
+      modal.classList.add("show");
+      modal.classList.add("fade");
+      modal.classList.remove("hide");
+      document.body.style.overflow = "hidden";
+    });
+  });
+  function closeModel() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+  }
+  close.addEventListener("click", closeModel);
+  modal.addEventListener("click", event => {
+    if (event.target === modal) {
+      closeModel();
+    }
+  });
+  document.addEventListener("keydown", e => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModel();
+    }
+  });
+});
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
